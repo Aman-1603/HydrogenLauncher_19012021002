c447554d9b710d057c255bea1eb3b2814db5667a
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2022-04-15 15:36:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hydrogenlauncher_19012021002/AppBlock.kt
@@ -0,0 +1,9 @@
+package com.example.hydrogenlauncher_19012021002
+
+import android.graphics.drawable.Drawable
+
+data class AppBlock (
+    val appName:String,
+    val icon: Drawable,
+    val packageName:String
+)
